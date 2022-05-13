17d0d98d2cdb76ffcf6945ac328779248ab896c5
DavidLordCaos <davidlucas807@yahoo.com.br>
2022-05-13 09:16:38 -0300

    removed files that not be used in dockerhub

--- config/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'event_path' => env('EVENT_MAP_PATH', base_path() . '/config/eventsMap.json'),
-    'webhook_key' => env('WEBHOOK_KEY', 'secret'),
-    'allowed_ips' => explode(',', env('ALLOWED_IPS', ['*'])),
-];
--- docker/scripts/laravelcron.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cd /root/automessage/ && docker-compose exec --user root php php /var/www/html/artisan schedule:run
